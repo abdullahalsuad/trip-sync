9b6851851156a1801b34131a6996e55a5194727e
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-11-15 22:36:29 +0600

    feat(auth) : add login API

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(registerDto: RegisterDto) {
     const { email, password } = registerDto;
@@ -33,5 +42,28 @@ export class AuthService {
     const { password: _, ...result } = newlyCreatedUser;
     void _;
     return result;
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    //find the current user by email as email is an unique property
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials! Please try again');
+    }
+
+    //verify the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials! Please try again');
+    }
+
+    const token = this.jwtService.sign({ userId: user.id });
+
+    const { password: _, ...result } = user;
+    void _;
+
+    return { ...result, token };
   }
 }
