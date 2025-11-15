9070f06febb02628cddc25aecc6f8cd21e9b6842
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-11-16 00:35:17 +0600

    fix(auth):fix login api

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+interface JwtPayload {
+  userId: string;
+  iat?: number;
+  exp?: number;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'JWT_SECRET',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return { userId: payload.userId };
+  }
+}
